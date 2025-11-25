52b97201039f027664ef135f17c67dda9118f34a
Jan Bautista <jan@g2c.dev>
2025-11-25 18:27:46 +0800

    Fixed Build

--- src/components/ui/theme-switcher.tsx
+++ src/components/ui/theme-switcher.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { useAppearance } from "@/hooks/use-appearance";
 import { Moon, Sun } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
